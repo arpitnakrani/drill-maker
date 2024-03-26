860ffc9e4bca86f1c6e3f974213e8000d225c89a
arpit <arpitnakrani2580@gmail.com>
2024-03-26 10:45:15 +0530

    image size set to 30*30 in canvas

--- app/page.tsx
+++ app/page.tsx
@@ -11,37 +11,25 @@ import { useEffect, useRef, useState } from "react";
 
 let currentShape: StraightArrow | null = null;
 export default function Home() {
-  const [actonTracker, setActonTracker] = useState<{ selectedMap: string, selectedTool: IDrillCurve | IDrillImage }>({
+  const [actionTracker, setActionTracker] = useState<{ selectedMap: string, selectedTool: IDrillCurve | IDrillImage }>({
     selectedMap: drillMaps[0].svgImagePath,
     selectedTool: toolsConfig['skate'][0]
   })
-  const [selectedMapPath, setSelectedMapPath] = useState(drillMaps[0].svgImagePath);
   const canvas_Ref_Main = useRef<HTMLCanvasElement | null>(null)
   const canvas_Ref_Temp = useRef<HTMLCanvasElement | null>(null)
   const isDrawing = useRef<boolean>(false)
 
   const onChangeTool = (tool: IDrillCurve | IDrillImage) => {
-    setActonTracker((prevAction) => ({ ...prevAction, selectedTool: tool }))
+    setActionTracker((prevAction) => ({ ...prevAction, selectedTool: tool }))
   }
 
   const startingPoints = useRef<{ x: number, y: number }>({
     x: 0,
     y: 0
   })
-  // useEffect(() => {
-  //   if (canvas_Ref_Main.current) {
-  //     const canvas_Ctx = canvas_Ref_Main.current.getContext('2d')
-  //     const img = document.createElement('img');
-  //     img.src = selectedMapPath;
-  //     img.onload = function () {
-  //       canvas_Ctx?.drawImage(img, 0, 0);
-  //     }
-  //     img.style.margin = 'auto'
-  //   }
-  // }, [selectedMapPath])
 
   const mouseDown: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
-    if (canvas_Ref_Temp.current) {
+    if (canvas_Ref_Temp.current && actionTracker.selectedTool.actionType === DrillActions.curve) {
       const rect = canvas_Ref_Temp.current.getBoundingClientRect();
       console.log(rect, 'temp can')
       currentShape = new StraightArrow(event.clientX - rect.left, event.clientY - rect.top, canvas_Ref_Temp.current)
@@ -75,16 +63,17 @@ export default function Home() {
   }
 
   const mouseClick: React.MouseEventHandler<HTMLCanvasElement> = (event) => {
-    if (actonTracker.selectedTool.actionType !== DrillActions.draw) return;
+    if (actionTracker.selectedTool.actionType !== DrillActions.draw) return;
 
     const mainCtx = canvas_Ref_Main.current?.getContext('2d');
     const tempCtx = canvas_Ref_Temp.current?.getContext('2d');
     if (!canvas_Ref_Temp.current || !mainCtx || !tempCtx) return; // Guard against null values
 
     const img = document.createElement('img');
-    img.src = actonTracker.selectedTool.imagePath;
+    const rect = canvas_Ref_Temp.current.getBoundingClientRect();
+    img.src = actionTracker.selectedTool.imagePath;
     img.onload = () => {
-      tempCtx.drawImage(img, event.clientX, event.clientY);
+      tempCtx.drawImage(img, event.clientX - rect.left, event.clientY - rect.top,30,30);
       if (canvas_Ref_Temp.current && canvas_Ref_Main.current) {
         mainCtx.drawImage(canvas_Ref_Temp.current, 0, 0); // Safe to use after the null check
         tempCtx.clearRect(0, 0, canvas_Ref_Main.current.width, canvas_Ref_Main.current.height);
@@ -96,7 +85,7 @@ export default function Home() {
       <div className="flex justify-center items-center gap-4 flex-wrap mt-4">
         {
           drillMaps.map((map, index) => <Tooltip text={map.label} key={index}>
-            <div className="group bg-gray-100 p-4 rounded-md h-24 w-24 m-auto cursor-pointer" onClick={() => setActonTracker((prevAction) => ({ ...prevAction, selectedMap: map.svgImagePath }))}>
+            <div className="group bg-gray-100 p-4 rounded-md h-24 w-24 m-auto cursor-pointer" onClick={() => setActionTracker((prevAction) => ({ ...prevAction, selectedMap: map.svgImagePath }))}>
               <Image src={map.svgImagePath} height={200} width={200} alt="full-rink" className="h-full m-auto" />
             </div>
           </Tooltip>
@@ -104,7 +93,7 @@ export default function Home() {
         }
       </div>
       <div className="relative">
-        <canvas id="drill_Canvas" height={496} width={992} className=" border-black mt-4" ref={canvas_Ref_Main} style={{ backgroundImage: `url(${actonTracker.selectedMap})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} />
+        <canvas id="drill_Canvas" height={496} width={992} className=" border-black mt-4" ref={canvas_Ref_Main} style={{ backgroundImage: `url(${actionTracker.selectedMap})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center' }} />
         <canvas id="drill_Canvas_Temp" height={496} width={992} className=" absolute left-0 top-0" ref={canvas_Ref_Temp}
           onMouseDown={mouseDown}
           onMouseMove={mouseMove}
@@ -113,7 +102,7 @@ export default function Home() {
           onClick={mouseClick}
         />
       </div>
-      <ToolSelection onToolChange={onChangeTool} selectedTool={actonTracker.selectedTool} />
+      <ToolSelection onToolChange={onChangeTool} selectedTool={actionTracker.selectedTool} />
     </main>
   );
 }
--- components/Tooltip.tsx
+++ components/Tooltip.tsx
@@ -9,7 +9,7 @@ const Tooltip = ({ children, text, position = 'bottom' }: {
     return (
         <div className="group relative flex justify-center">
             {children}
-            <div className={cn("absolute  scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 z-50", position === 'right' ? 'left-14' : '-bottom-10')}> {text}</div>
+            {/* <div className={cn("absolute  scale-0 rounded bg-gray-800 p-2 text-xs text-white group-hover:scale-100 z-50", position === 'right' ? 'left-14' : '-bottom-10')}> {text}</div> */}
         </div>
     );
 }
